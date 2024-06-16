ae76538386f6d3bac804b14d1399faf8b295d8d2
Kyle Stewart <kyle.b.stewart.gr@dartmouth.edu>
2024-06-16 18:26:55 -0400

    Add random computer choice function

---
+++ rockPaperScissors.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    choice = Math.random();
+    if (choice < 0.333) {
+        return "rock";
+    } else if (choice < 0.667) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
