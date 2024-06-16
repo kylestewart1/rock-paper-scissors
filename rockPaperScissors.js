6df49c89d0f825c4929b404241342d26317e6525
Kyle Stewart <kyle.b.stewart.gr@dartmouth.edu>
2024-06-16 18:55:22 -0400

    Add player input function with validation

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -9,4 +9,27 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    console.log("Choose one of the following: ");
+    console.log("1. Rock");
+    console.log("2. Paper");
+    console.log("3. Scissors");
+
+    let choice;
+
+    while (true) {
+        choice = prompt("Enter choice: ").toLowerCase();
+        if (choice == "1" || choice == "rock") {
+            return "rock";
+        } else if (choice == "2" || choice == "paper") {
+            return "paper";
+        } else if (choice == "3" || choice == "scissors") {
+            return "scissors";
+        } else {
+            console.log("Bruh");
+            alert("Come on, man. Enter a valid option.");
+        }
+    }
+}
+
+console.log(getHumanChoice());
